96a6439f9b956bb43dd50eaa1e8f466b3292f2a6
Mahesa Wijaya <mahesabw26@gmail.com>
2024-05-17 01:21:25 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,113 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="bg-black w-full h-auto px-[200px] py-[100px] flex justify-center">
+      <div className="flex flex-col w-full max-w-[1440px] h-auto space-y-14">
+        <header id="header" className="flex opacity-0 animate-slide-in">
+          <div id="pics" className="relative w-[300px] h-[300px]">
+            <Image className="object-cover object-center rounded-3xl" fill src={"/profile.jpg"} alt="pics" />
+          </div>
+          <div id="name" className="flex flex-col ml-20 justify-center">
+            <div className="text-white text-6xl w-[600px] font-bold font-Montserrat mb-[20px]"> MAHESA BIMA WIJAYA</div>
+            <div className="text-4xl font-bold text-blue-600 mb-[70px]">
+              Web Dev<span className="bg-blue-600 text-white pr-20">eloper</span>
+            </div>
+            <div className="flex space-x-4">
+              <div className="relative w-[40px] h-[40px]">
+                <Link href={"https://linkedin.com/in/mahesawijaya"} target="_blank">
+                  <Image src={"/linkedin.png"} fill alt="linkedin" />
+                </Link>
+              </div>
+              <div className="relative w-[40px] h-[40px]">
+                <Link href={"https://github.com/mahesabimawijaya"} target="_blank">
+                  <Image src={"/github.png"} fill alt="github" />
+                </Link>
+              </div>
+              <div className="relative w-[40px] h-[40px]">
+                <Link href={"https://wa.me/+6281384838398"} target="_blank">
+                  <Image src={"/whatsapp.png"} fill alt="whatsapp" />
+                </Link>
+              </div>
+              <div className="relative w-[40px] h-[40px]">
+                <Link href={"mailto:mahesabw26@gmail.com"} target="_blank">
+                  <Image src={"/gmail.png"} fill alt="gmail" />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </header>
+        <div id="body" className="border border-white text-white w-full h-auto rounded-md px-[40px] py-[30px]">
+          <div id="profile" className="flex flex-col space-y-5 mb-10">
+            <div className="text-3xl font-semibold">PROFILE</div>
+            <p className="text-justify">
+              Hello, I am Mahesa Bima Wijaya, a web development enthusiast. Recently, I graduated from a Fullstack Web Development bootcamp. I can do both frontend and backend programming, but I
+              prefer frontend because it&apos;s more visually appealing to me. Don&apos;t get me wrong, I still have good logic, therefore, I can do backend programming too.
+            </p>
+          </div>
+          <div id="skills" className="flex flex-col space-y-5 mb-20">
+            <div className="text-3xl font-semibold">SKILLS</div>
+            <div id="skills-container" className="flex flex-wrap justify-center">
+              <div className="relative w-[120px] h-[120px] mx-5">
+                <Image src={"/skills/html.png"} fill alt="html" />
+              </div>
+              <div className="relative w-[90px] h-[120px] mx-5">
+                <Image src={"/skills/css.png"} fill alt="css" />
+              </div>
+              <div className="relative w-[100px] h-[100px] m-5">
+                <Image src={"/skills/javascript.png"} fill alt="javascript" />
+              </div>
+              <div className="relative w-[150px] h-[100px] mt-5">
+                <Image src={"/skills/reactjs.png"} fill alt="reactjs" />
+              </div>
+              <div className="relative w-[140px] h-[100px] mt-5">
+                <Image src={"/skills/tailwind.png"} fill alt="tailwind" />
+              </div>
+              <div className="relative w-[100px] h-[100px] m-5">
+                <Image src={"/skills/nextjs.png"} fill alt="nextjs" />
+              </div>
+              <div className="relative w-[150px] h-[100px] m-5">
+                <Image src={"/skills/nodejs.png"} fill alt="nodejs" />
+              </div>
+              <div className="relative w-[300px] h-[100px] m-5">
+                <Image src={"/skills/expressjs.png"} fill alt="expressjs" />
+              </div>
+              <div className="relative w-[250px] h-[80px] m-5">
+                <Image src={"/skills/prisma.png"} fill alt="prisma" />
+              </div>
+              <div className="relative w-[200px] h-[100px] m-5">
+                <Image src={"/skills/mysql.png"} fill alt="mysql" />
+              </div>
+              <div className="relative w-[100px] h-[100px] m-5">
+                <Image src={"/skills/postgresql.png"} fill alt="postgresql" />
+              </div>
+            </div>
+          </div>
+          <div id="education" className="text-white flex flex-col space-y-5 mb-10">
+            <div className="text-3xl font-semibold">EDUCATION</div>
+            <div className="flex space-x-5">
+              <div className="relative w-20 h-20">
+                <Image className="rounded-md" src={"/educations/purwadhika.jpeg"} fill alt="purwadhika" />
+              </div>
+              <div className="flex flex-col">
+                <div className="font-semibold text-xl">Purwadhika Digital Technology School</div>
+                <div className="text-slate-300 italic">Full Stack Web Development</div>
+                <div>85.95</div>
+              </div>
+            </div>
+            <div className="flex space-x-5">
+              <div className="relative w-20 h-20">
+                <Image className="rounded-md" src={"/educations/sman1cikande.png"} fill alt="sman 1 cikande" />
+              </div>
+              <div className="flex flex-col">
+                <div className="font-semibold text-xl">SMAN 1 Cikande</div>
+                <div className="text-slate-300 italic">MIPA</div>
+                <div>89.98</div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
